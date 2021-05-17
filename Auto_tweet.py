b6fc08ff87c0a77bf8d831bc3013b4b5ca61db82
Divyanshu Singh <59568700+divyanshu887@users.noreply.github.com>
2021-05-17 11:30:55 +0530

    Update Auto_tweet.py

--- Auto_tweet.py
+++ Auto_tweet.py
@@ -1,10 +1,9 @@
-from logging import exception
 import tweepy
-
-consumer_key= "lExvivcHfGFaeX1xFtpv0m32J"
-consumer_secret_key= "hTkwaG4UeZGo1uusVUBIwStHBb4OcSahPAeGx8K9sSqTpu2DHw"
-access_token= "1394145443907440640-TwtVavBoZUCIhzG78qUQIvy5U1cT8q"
-access_token_secret= "FQt1i7kDlYVMihktJ4DAyPgxuD1gVONpXKQqxDIHC78jb"
+ # Enter the credentials -->
+consumer_key= ""
+consumer_secret_key= ""
+access_token= ""
+access_token_secret= ""
 
 def OAuth():
     try:
@@ -17,5 +16,5 @@ def OAuth():
 
 oauth=OAuth()
 api=tweepy.API(oauth)
-api.update_status("how is the weather")
-print("mojo introduced")
+api.update_status("Enter Your Message")
+print("Tweet is done!")
